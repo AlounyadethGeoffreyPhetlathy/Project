90d7695b721ffe5d2f2727816d78560690a33c80
AlounyadethGeoffreyPhetlathy <102637451+AlounyadethGeoffreyPhetlathy@users.noreply.github.com>
2022-11-17 19:32:47 -0600

    Setting up the quiz

---
+++ Main/App.js
@@ -0,0 +1,6 @@
+let A = document.getElementById("submitA");
+let B = document.getElementById("submitB");
+let C = document.getElementById("submitC");
+let D = document.getElementById("submitD");
+let E = document.getElementById("submitE");
+let F = document.getElementById("submitF");
